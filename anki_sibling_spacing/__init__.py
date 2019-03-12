a41d35d99702da6d7018c083c34dcc19f1575665
Anton Alekseev <anton@alekseev.cc>
2019-03-12 20:50:30 +0300

    Format with black

--- anki_sibling_spacing/__init__.py
+++ anki_sibling_spacing/__init__.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
-#-*- coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 # ---------------------------------------------------------------------------
 # Sibling Spacing is an addon for Anki 2 - http://ankisrs.net
 # ---------------------------------------------------------------------------
@@ -23,16 +23,20 @@ __version__ = 0.02
 
 # --- Functions: ---
 
+
 def siblingIvl(self, card, idealIvl, _old):
     origIvl = _old(self, card, idealIvl)
     ivl = origIvl
 
     # Penalty
-    minIvl = self.col.db.scalar('''SELECT MIN(ivl) FROM cards WHERE ivl > 0 AND nid = ? AND id != ? AND queue = 2''',
-                                card.nid, card.id)
+    minIvl = self.col.db.scalar(
+        """SELECT MIN(ivl) FROM cards WHERE ivl > 0 AND nid = ? AND id != ? AND queue = 2""",
+        card.nid,
+        card.id,
+    )
 
-    while minIvl and minIvl > 0 and ivl > minIvl*4:
-        ivl = max(1, int(ivl/2.0))
+    while minIvl and minIvl > 0 and ivl > minIvl * 4:
+        ivl = max(1, int(ivl / 2.0))
 
     # Boost
     delta = max(1, int(ivl * 0.15))
@@ -41,23 +45,37 @@ def siblingIvl(self, card, idealIvl, _old):
     siblings = True
 
     while siblings:
-        siblings = self.col.db.scalar('''SELECT count() FROM cards WHERE due >= ? AND due <= ? AND nid = ? AND id != ? AND queue = 2''',
-                                      self.today + ivl - delta, self.today + ivl + delta, card.nid, card.id)
+        siblings = self.col.db.scalar(
+            """SELECT count() FROM cards WHERE due >= ? AND due <= ? AND nid = ? AND id != ? AND queue = 2""",
+            self.today + ivl - delta,
+            self.today + ivl + delta,
+            card.nid,
+            card.id,
+        )
         if siblings:
             ivl += boost
 
     if config["Debug"]:
         if minIvl and minIvl > 0:
-            print("Sibling Spacing %d%+d = %d days for card %d (sibling has %d days)" % (origIvl,ivl-origIvl,ivl,card.id,minIvl,))
+            print(
+                "Sibling Spacing %d%+d = %d days for card %d (sibling has %d days)"
+                % (origIvl, ivl - origIvl, ivl, card.id, minIvl)
+            )
         else:
-            print("Sibling Spacing == %d days for card %d without visible siblings" % (ivl,card.id,))
+            print(
+                "Sibling Spacing == %d days for card %d without visible siblings"
+                % (ivl, card.id)
+            )
 
     return ivl
 
+
 def profileLoaded():
     # add scheduler
-    anki.sched.Scheduler._adjRevIvl = wrap(anki.sched.Scheduler._adjRevIvl,
-                                           siblingIvl, "around")
+    anki.sched.Scheduler._adjRevIvl = wrap(
+        anki.sched.Scheduler._adjRevIvl, siblingIvl, "around"
+    )
+
 
 # --- Hooks: ---
 
